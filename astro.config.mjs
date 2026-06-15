182a00a92a7c2491c4c886f2c7ce5dea3166190e
Mahin Utsman Nawawi <nawawimahinutsman@gmail.com>
2026-06-15 16:31:05 +0700

    Fix: Update site URL to custom domain https://undangan.ve-lora.my.id

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  site: "http://127.0.0.1:4321",
+  site: "https://undangan.ve-lora.my.id",
   integrations: [react(), sitemap()],
   output: "server",
   adapter: vercel(),
